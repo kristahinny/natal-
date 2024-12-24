8ad8a6ae101a42674de24a96667f01e87676fb56
kristahinny <162491492+kristahinny@users.noreply.github.com>
2024-12-24 10:29:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Função para mostrar a foto
+function mostrarFoto() {
+    // Obtenha a div que contém a foto
+    var foto = document.getElementById("foto");
+
+    // Altere o estilo para exibir a foto
+    foto.style.display = "block";
+}
+
+// Função para ocultar a foto
+function ocultarFoto() {
+    // Obtenha a div que contém a foto
+    var foto = document.getElementById("foto");
+
+    // Altere o estilo para esconder a foto
+    foto.style.display = "none";
+}
